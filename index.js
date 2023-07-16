00b8ec53110d4514eb0ba3f4e3bc42347db8aa4a
--global <seraji.maryam1982@gmail.com>
2023-07-17 02:04:01 +0430

    3 js files created

---
+++ index.js
@@ -0,0 +1,14 @@
+let color_array ='blue, red , green , black'
+let word=''
+
+for (i=0;i<color_array.length;i++){
+    if (color_array[i]!=','){
+        word=word+=color_array[i]
+    }
+    else{
+        word=word+=color_array[i]
+    }
+    
+    
+}
+console.log(word)
---
+++ index2.js
@@ -0,0 +1,10 @@
+const color_array ='   blue, red , green , black     '
+let word=''
+let i=0
+let j=color_array.length
+
+while (color_array[i]) {
+    
+}
+    
+    
